28bb091deed680d99f79cfee2221f5fc341b66fd
Morra13 <55277914+Morra13@users.noreply.github.com>
2024-06-04 13:51:35 +0400

    #22 access not empty

--- www/resources/views/access/myAccess.blade.php
+++ www/resources/views/access/myAccess.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app', ['title' => __('Мои доступ')])
-
 @section('content')
+    <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold text-body-emphasis">Ваши доступы</h1>
+        @if(empty($arTodo))
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4 text-danger">У вас нет доступов</p>
+            </div>
+        @endif
+    </div>
     @foreach($arTodo as $todo)
         @include('access.accessRow', ['todo' => $todo])
     @endforeach
